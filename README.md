dc209098bf6b7d65a18a76ddc9d398eb972d541d
Daniel Bacab <Daniel.Bacab@itksquare.edu.mx>
2022-10-21 19:27:26 -0500

    first exercise done

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # ui_practice
 Daniel Bacab - UI Practice
+
+## Practice 1
+![Practice 1](./screenshots/practice_1.png)
---
+++ lib/screens/practice_1.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+//import 'package:flutter/src/widgets/container.dart';
+//import 'package:flutter/src/widgets/framework.dart';
+
+class Practice1 extends StatelessWidget {
+  const Practice1({super.key});
+
+  @override 
+  Widget build(BuildContext context){
+    return const Scaffold(
+      backgroundColor: Colors.black,
+      body: Center(
+        child: Text(
+          "Hello Flutter!\n  I am Daniel",
+          style: TextStyle(
+              fontSize: 30,
+              fontWeight: FontWeight.w800,
+              color: Colors.white,
+          ),
+        ),
+      ),
+    );
+  }
+}
